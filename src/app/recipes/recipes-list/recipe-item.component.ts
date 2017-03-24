a161805751e8facf415dfc555545351221f85ad2
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-24 12:28:48 +0200

    added recipe component imported recipe component added types accourdingly

--- src/app/recipes/recipes-list/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-item',
   templateUrl: './recipe-item.component.html',
 })
 export class RecipeItemComponent implements OnInit {
-recipe;
-  recipeId;
+  recipe: Recipe;
+  recipeId: number;
+  
   constructor() { }
 
   ngOnInit() {
