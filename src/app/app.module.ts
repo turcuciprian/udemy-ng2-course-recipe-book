8b6145de3ef679508f487c402654f00a2f9b3724
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-24 12:06:52 +0200

    moved recipes files into their own directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
